babe69dfa768b2792592b0ffcfd652e4ea51524a
Benjamin Laverman <benlaverman@gmail.com>
2020-10-07 22:07:27 -0700

    Updated all the functions to reflect the assigment guidelines

--- cse360assignment02/src/cse360assignment02/AddingMachine.java
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -2,25 +2,45 @@ package cse360assignment02;
 
 public class AddingMachine {
   private int total;
+  private String str = "0";
   
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
   }
-  
+  /**
+   * Get the total
+   * @return the total
+   */
   public int getTotal () { // returns the total
-    return 0;
+    return total;
   }
-  
+  /**
+   * Adds the value to the total
+   * @param value is added to the total
+   */
   public void add (int value) { 
+	  str += " + " + value;
+	  total += value;
   }
-
+  /**
+   * subtracts value from the total
+   * @param value is subtracted from the total
+   */
   public void subtract (int value) {
+	  str += " - " + value;
+	  total -= value;
   }
-
+  /**
+   * To String method converts to string
+   */
   public String toString () {
-    return "";
+    return str;
   }
-
+  /**
+   * clears the Machine
+   */
   public void clear() {
+	  total = 0;
+	  str = "0";
   }
 }
